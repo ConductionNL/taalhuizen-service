e72a9abf40e55e2fd544867a20941634ac0254e6
Wilco Louwerse <wilco@conduction.nl>
2021-04-08 16:04:01 +0200

    Learning need update

--- api/src/Entity/LearningNeed.php
+++ api/src/Entity/LearningNeed.php
@@ -30,175 +30,263 @@ class LearningNeed
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $description;
+    private $learningNeedDescription;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $motivation;
+    private $learningNeedMotivation;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $learningNeedVerb;
+    private $desiredOutComesGoal;
+
+    /**
+     * @Assert\Choice({"DUTCH_READING", "DUTCH_WRITING", "MATH_NUMBERS", "MATH_PROPORTION", "MATH_GEOMETRY", "MATH_LINKS", "DIGITAL_USING_ICT_SYSTEMS", "DIGITAL_SEARCHING_INFORMATION", "DIGITAL_PROCESSING_INFORMATION", "DIGITAL_COMMUNICATION", "KNOWLEDGE", "SKILLS", "ATTITUDE", "BEHAVIOUR", "OTHER"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $desiredOutComesTopic;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $learningNeedSubject;
+    private $desiredOutComesTopicOther;
+
+    /**
+     * @Assert\Choice({"FAMILY_AND_PARENTING", "LABOR_MARKET_AND_WORK", "HEALTH_AND_WELLBEING", "ADMINISTRATION_AND_FINANCE", "HOUSING_AND_NEIGHBORHOOD", "SELFRELIANCE", "OTHER"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $desiredOutComesApplication;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $learningNeedApplication;
+    private $desiredOutComesApplicationOther;
+
+    /**
+     * @Assert\Choice({"INFLOW", "NLQF1", "NLQF2", "NLQF3", "NLQF4", "OTHER"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $desiredOutComesLevel;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $learningNeedLevel;
+    private $desiredOutComesLevelOther;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $desiredOffer;
+    private $offerDesiredOffer;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $advisedOffer;
+    private $offerAdvisedOffer;
 
     /**
      * @Assert\Choice({"Nee, er is geen verschil", "Ja, want: niet aangeboden binnen bereisbare afstand", "Ja, want: wachtlijst", "Ja, want:anders"})
      * @ORM\Column(type="string", length=255)
      */
-    private $differenceDesireAdvice;
+    private $offerDifference;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $offerDifferenceOther;
 
     /**
      * @ORM\Column(type="string", length=2550, nullable=true)
      */
-    private $agreementsNote;
+    private $offerEngagements;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $participations = [];
 
     public function getId(): Uuid
     {
         return $this->id;
     }
 
-    public function getDescription(): ?string
+    public function getLearningNeedDescription(): ?string
     {
-        return $this->description;
+        return $this->learningNeedDescription;
     }
 
-    public function setDescription(string $description): self
+    public function setLearningNeedDescription(string $learningNeedDescription): self
     {
-        $this->description = $description;
+        $this->learningNeedDescription = $learningNeedDescription;
 
         return $this;
     }
 
-    public function getMotivation(): ?string
+    public function getLearningNeedMotivation(): ?string
     {
-        return $this->motivation;
+        return $this->learningNeedMotivation;
     }
 
-    public function setMotivation(string $motivation): self
+    public function setLearningNeedMotivation(string $learningNeedMotivation): self
     {
-        $this->motivation = $motivation;
+        $this->learningNeedMotivation = $learningNeedMotivation;
 
         return $this;
     }
 
-    public function getLearningNeedVerb(): ?string
+    public function getDesiredOutComesGoal(): ?string
     {
-        return $this->learningNeedVerb;
+        return $this->desiredOutComesGoal;
     }
 
-    public function setLearningNeedVerb(string $learningNeedVerb): self
+    public function setDesiredOutComesGoal(string $desiredOutComesGoal): self
     {
-        $this->learningNeedVerb = $learningNeedVerb;
+        $this->desiredOutComesGoal = $desiredOutComesGoal;
 
         return $this;
     }
 
-    public function getLearningNeedSubject(): ?string
+    public function getDesiredOutComesTopic(): ?string
     {
-        return $this->learningNeedSubject;
+        return $this->desiredOutComesTopic;
     }
 
-    public function setLearningNeedSubject(string $learningNeedSubject): self
+    public function setDesiredOutComesTopic(string $desiredOutComesTopic): self
     {
-        $this->learningNeedSubject = $learningNeedSubject;
+        $this->desiredOutComesTopic = $desiredOutComesTopic;
 
         return $this;
     }
 
-    public function getLearningNeedApplication(): ?string
+    public function getDesiredOutComesTopicOther(): ?string
     {
-        return $this->learningNeedApplication;
+        return $this->desiredOutComesTopicOther;
     }
 
-    public function setLearningNeedApplication(string $learningNeedApplication): self
+    public function setDesiredOutComesTopicOther(string $desiredOutComesTopicOther): self
     {
-        $this->learningNeedApplication = $learningNeedApplication;
+        $this->desiredOutComesTopicOther = $desiredOutComesTopicOther;
 
         return $this;
     }
 
-    public function getLearningNeedLevel(): ?string
+    public function getDesiredOutComesApplication(): ?string
     {
-        return $this->learningNeedLevel;
+        return $this->desiredOutComesApplication;
     }
 
-    public function setLearningNeedLevel(string $learningNeedLevel): self
+    public function setDesiredOutComesApplication(string $desiredOutComesApplication): self
     {
-        $this->learningNeedLevel = $learningNeedLevel;
+        $this->desiredOutComesApplication = $desiredOutComesApplication;
 
         return $this;
     }
 
-    public function getDesiredOffer(): ?string
+    public function getDesiredOutComesApplicationOther(): ?string
     {
-        return $this->desiredOffer;
+        return $this->desiredOutComesApplicationOther;
     }
 
-    public function setDesiredOffer(string $desiredOffer): self
+    public function setDesiredOutComesApplicationOther(string $desiredOutComesApplicationOther): self
     {
-        $this->desiredOffer = $desiredOffer;
+        $this->desiredOutComesApplicationOther = $desiredOutComesApplicationOther;
 
         return $this;
     }
 
-    public function getAdvisedOffer(): ?string
+    public function getDesiredOutComesLevel(): ?string
     {
-        return $this->advisedOffer;
+        return $this->desiredOutComesLevel;
     }
 
-    public function setAdvisedOffer(string $advisedOffer): self
+    public function setDesiredOutComesLevel(string $desiredOutComesLevel): self
     {
-        $this->advisedOffer = $advisedOffer;
+        $this->desiredOutComesLevel = $desiredOutComesLevel;
 
         return $this;
     }
 
-    public function getDifferenceDesireAdvice(): ?string
+    public function getDesiredOutComesLevelOther(): ?string
     {
-        return $this->differenceDesireAdvice;
+        return $this->desiredOutComesLevelOther;
     }
 
-    public function setDifferenceDesireAdvice(string $differenceDesireAdvice): self
+    public function setDesiredOutComesLevelOther(string $desiredOutComesLevelOther): self
     {
-        $this->differenceDesireAdvice = $differenceDesireAdvice;
+        $this->desiredOutComesLevelOther = $desiredOutComesLevelOther;
 
         return $this;
     }
 
-    public function getAgreementsNote(): ?string
+    public function getOfferDesiredOffer(): ?string
     {
-        return $this->agreementsNote;
+        return $this->offerDesiredOffer;
     }
 
-    public function setAgreementsNote(?string $agreementsNote): self
+    public function setOfferDesiredOffer(string $offerDesiredOffer): self
     {
-        $this->agreementsNote = $agreementsNote;
+        $this->offerDesiredOffer = $offerDesiredOffer;
+
+        return $this;
+    }
+
+    public function getOfferAdvisedOffer(): ?string
+    {
+        return $this->offerAdvisedOffer;
+    }
+
+    public function setOfferAdvisedOffer(string $offerAdvisedOffer): self
+    {
+        $this->offerAdvisedOffer = $offerAdvisedOffer;
+
+        return $this;
+    }
+
+    public function getOfferDifference(): ?string
+    {
+        return $this->offerDifference;
+    }
+
+    public function setOfferDifference(string $offerDifference): self
+    {
+        $this->offerDifference = $offerDifference;
+
+        return $this;
+    }
+
+    public function getOfferDifferenceOther(): ?string
+    {
+        return $this->offerDifferenceOther;
+    }
+
+    public function setOfferDifferenceOther(string $offerDifferenceOther): self
+    {
+        $this->offerDifferenceOther = $offerDifferenceOther;
+
+        return $this;
+    }
+
+    public function getOfferEngagements(): ?string
+    {
+        return $this->offerEngagements;
+    }
+
+    public function setOfferEngagements(?string $offerEngagements): self
+    {
+        $this->offerEngagements = $offerEngagements;
+
+        return $this;
+    }
+
+    public function getParticipations(): ?array
+    {
+        return $this->participations;
+    }
+
+    public function setParticipations(?array $participations): self
+    {
+        $this->participations = $participations;
 
         return $this;
     }
