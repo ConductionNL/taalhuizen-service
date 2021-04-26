bc9dbe833b95dfae3ee19124201df7859ec332c9
Mark Bout <mark@conduction.nl>
2021-04-26 12:30:22 +0200

    wip groups

--- api/src/Resolver/GroupMutationResolver.php
+++ api/src/Resolver/GroupMutationResolver.php
@@ -7,6 +7,8 @@ namespace App\Resolver;
 use ApiPlatform\Core\GraphQl\Resolver\MutationResolverInterface;
 use App\Entity\Address;
 use App\Entity\Group;
+use App\Service\EAVService;
+use App\Service\EDUService;
 use App\Entity\LanguageHouse;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,9 +19,13 @@ class GroupMutationResolver implements MutationResolverInterface
 {
 
     private EntityManagerInterface $entityManager;
+    private EAVService $eavService;
+    private EDUService $eduService;
 
-    public function __construct(EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager, EAVService $eavService, EDUService $eduService){
         $this->entityManager = $entityManager;
+        $this->eavService = $eavService;
+        $this->eduService = $eduService;
     }
     /**
      * @inheritDoc
@@ -45,6 +51,9 @@ class GroupMutationResolver implements MutationResolverInterface
 
     public function createGroup(array $groupArray): Group
     {
+        $result['result'] = [];
+
+        $group = $this->dtoToGroup($groupArray);
         $group = new Group();
         $this->entityManager->persist($group);
         return $group;
@@ -70,5 +79,55 @@ class GroupMutationResolver implements MutationResolverInterface
     {
 
         return null;
+    }
+
+    public function dtoToGroup(Group $resource){
+        if ($resource->getId()){
+            $group['id'] = $resource->getId();
+        }
+        $group['aanbiederId'] = $resource->getAanbiederId();
+        $group['name'] = $resource->getName();
+        $group['typeCourse'] = $resource->getTypeCourse();
+        $group['outComesGoal'] = $resource->getOutComesGoal();
+        $group['outComesTopic'] =$resource->getOutComesTopic();
+        if ($resource->getOutComesTopicOther()){
+            $group['outComesTopicOther'] = $resource->getOutComesTopicOther();
+        }
+        $group['outComesApplication'] = $resource->getOutComesApplication();
+        if ($resource->getOutComesApplicationOther()){
+            $group['outComesApplicationOther'] = $resource->getOutComesApplicationOther();
+        }
+        $group['outComesLevel'] = $resource->getOutComesLevel();
+        if ($resource->getOutComesLevelOther()){
+            $group['outComesLevelOther'] = $resource->getOutComesLevelOther();
+        }
+        $group['detailsIsFormal'] = $resource->getDetailsIsFormal();
+        $group['detailsTotalClassHours'] = $resource->getDetailsTotalClassHours();
+        $group['detailsCertificateWillBeAwarded'] = $resource->getDetailsCertificateWillBeAwarded();
+        if ($resource->getDetailsStartDate()) {
+            $group['detailsStartDate'] = $resource->getDetailsStartDate();
+        }
+        if ($resource->getDetailsEndDate()){
+            $group['detailsEndDate'] = $resource->getDetailsEndDate();
+        }
+        if ($resource->getAvailability()){
+            $group['availability'] = $resource->getAvailability();
+        }
+        if ($resource->getAvailabilityNotes()){
+            $group['availabilityNotes'] = $resource->getAvailabilityNotes();
+        }
+        $group['generalLocation'] = $resource->getGeneralLocation();
+        if ($resource->getGeneralParticipantsMin()) {
+            $group['generalParticipantsMin'] = $resource->getGeneralParticipantsMin();
+        }
+        if ($resource->getGeneralParticipantsMax()){
+            $group['generalParticipantsMax'] = $resource->getGeneralParticipantsMax();
+        }
+        if ($resource->getGeneralEvaluation()){
+            $group['generalEvaluation'] = $resource->getGeneralEvaluation();
+        }
+        $group['aanbiederEmployeeIds'] = $resource->getAanbiederEmployeeIds();
+
+        return $group;
     }
 }
