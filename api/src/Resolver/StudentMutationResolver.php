92fd63fc3010c7f2ef2d095b0d3d7a665ca46952
Wilco Louwerse <wilco@conduction.nl>
2021-06-08 09:37:12 +0200

    add postman collection

--- api/src/Resolver/StudentMutationResolver.php
+++ api/src/Resolver/StudentMutationResolver.php
@@ -252,7 +252,6 @@ class StudentMutationResolver implements MutationResolverInterface
         return $memo;
     }
 
-    /w.i.p
     private function inputToPerson(array $input, string $languageHouseId = null, $updatePerson = null)
     {
         if (isset($languageHouseId)) {
