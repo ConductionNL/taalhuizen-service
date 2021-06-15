15aebd83df58f15bdc42602567f6cde7d5831535
Wilco Louwerse <WilcoLouwerse@users.noreply.github.com>
2021-06-15 13:26:51 +0000

    Apply fixes from StyleCI

--- api/tests/Service/ResolverServiceTest.php
+++ api/tests/Service/ResolverServiceTest.php
@@ -7,22 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ResolverServiceTest extends KernelTestCase
 {
-
     private $serviceContainer;
     private $resolverService;
 
     protected function setUp(): void
     {
-
         self::bootKernel();
         $this->serviceContainer = self::$container;
         $this->resolverService = $this->serviceContainer->get(ResolverService::class);
         parent::setUp();
     }
 
-
     public function testCreatePaginator()
     {
-
     }
 }
