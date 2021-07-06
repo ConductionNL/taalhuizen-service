07c29f8de7374a50289534435ac806849f6941a8
Sarai Misidjan <sarai@conduction.nl>
2021-07-06 09:59:19 +0200

    fix Organization entity

--- api/src/Entity/Organization.php
+++ api/src/Entity/Organization.php
@@ -1,108 +1,47 @@
 <?php
 
-
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
-use App\Resolver\LanguageHouseMutationResolver;
-use App\Resolver\LanguageHouseQueryCollectionResolver;
-use App\Resolver\LanguageHouseQueryItemResolver;
-use DateTime;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Inflector\Inflector;
+use App\Repository\OrganizationRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Serializer\Annotation\MaxDepth;
-use Symfony\Component\Validator\Constraints as Assert;
 
-
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=OrganizationRepository::class)
+ */
 class Organization
 {
     /**
-     * @var UuidInterface The UUID identifier of this resource
-     *
-     * @example e2984465-190a-4562-829e-a8cca81aa35d
-     *
      * @ORM\Id
-     * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
-     * @var string The Name of this resource.
-     *
-     * @Assert\Length(
-     *     max = 255
-     * )
-     * @Assert\NotNull
-     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255)
      */
-    private string $name;
+    private $name;
 
     /**
-     * @var string The Telephone of this resource.
-     *
-     * @Assert\Length(
-     *     max = 255
-     * )
-     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $telephone;
 
     /**
-     * @var string The Email of this resource.
-     *
-     * @Assert\Length(
-     *     max = 2550
-     * )
-     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $email;
 
     /**
-     * @var string Type resource
-     *
-     * @Assert\Length(
-     *     max = 255
-     * )
-     * @Groups({"write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $type;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=Address::class)
-     */
-    private $addresses;
-
-    public function __construct()
-    {
-        $this->addresses = new ArrayCollection();
-    }
-
-    public function getId(): ?UuidInterface
+    public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(?UuidInterface $uuid): self
-    {
-        $this->id = $uuid;
-        return $this;
     }
 
     public function getName(): ?string
@@ -146,35 +85,10 @@ class Organization
         return $this->type;
     }
 
-    public function setType(string $type): self
+    public function setType(?string $type): self
     {
         $this->type = $type;
 
         return $this;
     }
-
-    /**
-     * @return Collection|Address[]
-     */
-    public function getAddresses(): Collection
-    {
-        return $this->addresses;
-    }
-
-    public function addAddress(Address $address): self
-    {
-        if (!$this->addresses->contains($address)) {
-            $this->addresses[] = $address;
-        }
-
-        return $this;
-    }
-
-    public function removeAddress(Address $address): self
-    {
-        $this->addresses->removeElement($address);
-
-        return $this;
-    }
-
 }
