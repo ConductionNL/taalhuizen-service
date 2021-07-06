ee5ff239e64920160ac913a4590bad024774feca
Sarai Misidjan <sarai@conduction.nl>
2021-07-06 10:51:47 +0200

    Person/Organization/Telephone/Email entities added

--- api/src/Entity/Person.php
+++ api/src/Entity/Person.php
@@ -2,11 +2,23 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PersonRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
@@ -15,50 +27,99 @@ use Doctrine\ORM\Mapping as ORM;
 class Person
 {
     /**
+     * @var UuidInterface The UUID identifier of this telephone
+     *
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
+     *
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
     private $id;
 
     /**
+     * @var string Given name of this person
+     *
+     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255)
      */
-    private $givenName;
+    private string $givenName;
 
     /**
+     * @var ?string Additional name of this person
+     *
+     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $additionalName;
+    private ?string $additionalName;
 
     /**
+     * @var ?string Family name of this person
+     *
+     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $familyName;
+    private ?string $familyName;
 
     /**
+     * @var ?string Gender of this person
+     *
+     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $gender;
+    private ?string $gender;
 
     /**
+     * @var ?string Birthday of this person
+     *
+     * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $birthday;
+    private ?string $birthday;
 
     /**
-     * @ORM\ManyToMany(targetEntity=Address::class)
+     * @var ?Address Address of this person
+     *
+     * @Groups({"read", "write"})
+     * @ORM\OneToOne(targetEntity=Address::class, cascade={"persist", "remove"})
      */
-    private $addresses;
+    private ?Address $addresses;
 
-    public function __construct()
-    {
-        $this->addresses = new ArrayCollection();
-    }
+    /**
+     * @var ?Telephone Telephone of this person
+     *
+     * @Groups({"read", "write"})
+     * @ORM\OneToOne(targetEntity=Telephone::class, cascade={"persist", "remove"})
+     */
+    private ?Telephone $telephones;
 
-    public function getId(): ?int
+    /**
+     * @var ?Email Email of this person
+     *
+     * @Groups({"read", "write"})
+     * @ORM\OneToOne(targetEntity=Email::class, cascade={"persist", "remove"})
+     */
+    private ?Email $emails;
+
+    /**
+     * @var ?Organization Organization of this person
+     *
+     * @Groups({"read", "write"})
+     * @ORM\OneToOne(targetEntity=Organization::class, cascade={"persist", "remove"})
+     */
+    private ?Organization $organization;
+
+    public function getId(): ?UuidInterface
     {
         return $this->id;
+    }
+
+    public function setId(?UuidInterface $uuid): self
+    {
+        $this->id = $uuid;
+
+        return $this;
     }
 
     public function getGivenName(): ?string
@@ -121,26 +182,50 @@ class Person
         return $this;
     }
 
-    /**
-     * @return Collection|Address[]
-     */
-    public function getAddresses(): Collection
+    public function getAddresses(): ?Address
     {
         return $this->addresses;
     }
 
-    public function addAddress(Address $address): self
+    public function setAddresses(?Address $addresses): self
     {
-        if (!$this->addresses->contains($address)) {
-            $this->addresses[] = $address;
-        }
+        $this->addresses = $addresses;
 
         return $this;
     }
 
-    public function removeAddress(Address $address): self
+    public function getTelephones(): ?Telephone
     {
-        $this->addresses->removeElement($address);
+        return $this->telephones;
+    }
+
+    public function setTelephones(?Telephone $telephones): self
+    {
+        $this->telephones = $telephones;
+
+        return $this;
+    }
+
+    public function getEmails(): ?Email
+    {
+        return $this->emails;
+    }
+
+    public function setEmails(?Email $emails): self
+    {
+        $this->emails = $emails;
+
+        return $this;
+    }
+
+    public function getOrganization(): ?Organization
+    {
+        return $this->organization;
+    }
+
+    public function setOrganization(?Organization $organization): self
+    {
+        $this->organization = $organization;
 
         return $this;
     }
