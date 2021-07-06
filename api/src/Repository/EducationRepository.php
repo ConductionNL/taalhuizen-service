636e79dbc5e71d1917f7f47a9f0caa8659ea8d00
Sarai Misidjan <sarai@conduction.nl>
2021-07-06 12:29:59 +0200

    Added education entity

---
+++ api/src/Repository/EducationRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Education;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method EducationRepository|null find($id, $lockMode = null, $lockVersion = null)
+ * @method EducationRepository|null findOneBy(array $criteria, array $orderBy = null)
+ * @method EducationRepository[]    findAll()
+ * @method EducationRepository[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EducationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, EducationRepository::class);
+    }
+
+    // /**
+    //  * @return EducationRepository[] Returns an array of EducationRepository objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?EducationRepository
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
